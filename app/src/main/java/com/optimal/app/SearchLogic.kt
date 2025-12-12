f929ad108a7de55f937bd127aa74fc3c9dffdf2c
celestisovranone-debug <celestisovranone@gmail.com>
2025-12-13 01:29:32 +0530

    Create SearchLogic.kt

---
+++ app/src/main/java/com/optimal/app/SearchLogic.kt
@@ -0,0 +1,8 @@
+package com.optimal.app
+
+object SearchLogic {
+    fun parse(query: String): String {
+        // placeholder: later this will parse numbers + keywords
+        return query.trim()
+    }
+}
