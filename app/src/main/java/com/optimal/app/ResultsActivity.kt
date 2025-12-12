7c9fdde023be802011385d5d3d4b0a8e265eb855
celestisovranone-debug <celestisovranone@gmail.com>
2025-12-13 01:27:30 +0530

    Create ResultsActivity.kt

---
+++ app/src/main/java/com/optimal/app/ResultsActivity.kt
@@ -0,0 +1,11 @@
+package com.optimal.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results)
+    }
+}
